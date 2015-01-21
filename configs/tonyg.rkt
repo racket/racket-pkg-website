aea422907e1da8bd7c9a87c79a1c58f3c146c817
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-01-21 16:02:16 -0500

    More local dev config

--- configs/tonyg.rkt
+++ configs/tonyg.rkt
@@ -3,4 +3,9 @@
 (require "../src/main.rkt")
 (main (hash 'port 8444
             'reloadable? #t
+            'package-index-url "http://localhost/~tonyg/pkg-catalog-static/pkgs-all.json.gz"
+            'static-cached-directory (build-path (find-system-path 'home-dir)
+                                                 "public_html/pkg-catalog-static")
+            'static-cached-urlprefix "http://localhost/~tonyg/pkg-catalog-static/"
+            'backend-baseurl "https://localhost:8445"
             ))
