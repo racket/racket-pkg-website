31627739dd15590381cfc7b8da71a15e2eca584f
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-08-09 16:46:26 -0400

    Dev setup for tonyg to run a local S3 proxy

--- configs/tonyg.rkt
+++ configs/tonyg.rkt
@@ -3,17 +3,25 @@
 (require "../src/main.rkt")
 (main (hash 'port 8444
             'reloadable? #t
-            'package-index-url "https://localhost:8444/pkgs-all.json.gz"
+            'package-index-url "file:///home/tonyg/public_html/pkg-index-static/pkgs-all.json.gz"
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
             ;; Either:
+            ;;
             'static-output-type 'file
             'static-content-target-directory (build-path (find-system-path 'home-dir)
                                                          "public_html/pkg-catalog-static")
+            'static-urlprefix "https://localhost/~tonyg/pkg-catalog-static"
+            ;;
             ;; Or:
+            ;;
             ;; 'static-output-type 'aws-s3
             ;; 'aws-s3-bucket+path "pkgs.leastfixedpoint.com/"
+            ;; ;; These two should be set to an HTTPS proxy (e.g. nginx) proxying to S3,
+            ;; ;; http://pkgs.leastfixedpoint.com.s3-website-us-east-1.amazonaws.com
+            ;; 'static-urlprefix "https://localhost:8446"
+            ;; 'dynamic-static-urlprefix "https://localhost:8446"
+            ;;
             ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-            'static-urlprefix "https://localhost/~tonyg/pkg-catalog-static"
             'dynamic-urlprefix "https://localhost:8444"
             'backend-baseurl "https://localhost:8445"
             'pkg-index-generated-directory (build-path (find-system-path 'home-dir)
