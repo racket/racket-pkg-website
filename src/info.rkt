c69054a7c6283b25f184ab0289819d311750f01c
Jack Firth <jackhfirth@gmail.com>
2017-05-07 17:24:34 -0700

    Add more missing deps

--- src/info.rkt
+++ src/info.rkt
@@ -1,7 +1,9 @@
 #lang info
 (define deps
   ;; 6.7 was released with an incorrect version number
-  '(("base" #:version "6.6.0.900")
+  '("web-server-lib"
+    ("base" #:version "6.6.0.900")
     "reloadable"
     "aws"))
 (define collection "pkg-website")
+(define build-deps '("rackunit-lib"))
