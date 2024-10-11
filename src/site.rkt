50cb3245a5574065ab4719944aaddcce7dba1eb7
Matthew Flatt <mflatt@racket-lang.org>
2024-10-11 05:53:55 -0600

    guard against a broken `implies` field

--- src/site.rkt
+++ src/site.rkt
@@ -647,7 +647,9 @@
       "N/A"))
 
 (define (get-implied-docs pkg)
-  (define implied-names (map string->symbol (package-implies pkg)))
+  (define implied-names (filter-map
+                         (lambda (p) (and (string? p) (string->symbol p)))
+                         (package-implies pkg)))
   (append-map package-docs (package-batch-detail implied-names)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
